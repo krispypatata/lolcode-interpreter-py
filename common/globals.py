c7769890f205d42b4f727f015f9ac8eda53bcf30
krispypatata <keithginoel@gmail.com>
2025-06-05 17:22:55 +0800

    chore: ADD global variables for lexemes and symbol table

--- common/globals.py
+++ common/globals.py
@@ -1,1 +1,4 @@
-no_gui = True
+no_gui = True        # Flag to disable GUI features
+
+symbol_table = None  # Storage for variables and functions and their values
+tokens = None  # Storage for tokens (list of tuples with token, tag, and line number)
